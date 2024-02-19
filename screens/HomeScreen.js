ce8c9e3d697e746a3f2a09dd849f60060c11ee57
MAC <mac@Clintons-MacBook-Air.local>
2024-02-19 15:24:33 +0100

    delivery app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,76 @@
+import { Image, Text, View, TextInput, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import CategoryCard from "../components/CategoryCard";
+import FeaturedRow from "../components/FeaturedRow";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View>
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-red-300 p-4 rounded-full"
+          />
+        </View>
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row items-center flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurant and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+        {/* FeaturedRows */}
+        <FeaturedRow id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        <FeaturedRow id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        <FeaturedRow id="12345"
+          title="Offers near you"
+          description="Why not support your local restaurant tonight!"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
