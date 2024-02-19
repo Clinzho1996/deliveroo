64878cba3bb1579a268e8d209ec1f1bc74c02e4b
MAC <mac@Clintons-MacBook-Air.local>
2024-02-19 21:30:22 +0100

    sanity

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import RestaurantScreen from "./screens/RestaurantScreen";
-import { store } from "./store";
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <NavigationContainer>
-      <Provider store={store}>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Restaurants" component={RestaurantScreen} />
-          </Stack.Navigator>
-      </Provider>
-    </NavigationContainer>
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Restaurant' component={RestaurantScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
+
